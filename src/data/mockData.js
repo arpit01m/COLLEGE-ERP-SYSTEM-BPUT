9cda665e0d57c1f9148afe393adfdd5a1357266c
Tanishka <tanishkakri5@gmail.com>
2026-09-17 16:56:47 +0530

    Update mockData.js

--- src/data/mockData.js
+++ src/data/mockData.js
@@ -308,7 +308,7 @@ export const INITIAL_EVENTS = [
     registrationDeadline: "2026-10-25",
     status: "Published",
     featured: false,
-    banner: "https://images.unsplash.com/photo-1531403009284-440f080d1e12?auto=format&fit=crop&w=800&q=80",
+    banner: "/images/bput_hackathon.jpeg",
     description: "Exhibition of student patents, research papers published in IEEE/Springer, and prototype hardware demos funded by Govt of Odisha.",
     externalLink: "",
     participants: ["2501445597", "2501445229"]
